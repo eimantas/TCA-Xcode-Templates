d34e9e7015f0cac3ed1a5528de18260c41356f7b
Eimantas Vaičiūnas <eimantas@stork.software>
2020-11-25 09:09:36 +0200

    Initial commit

---
+++ Feature.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+import ComposableArchitecture
+
+struct ___VARIABLE_feature___State: Equatable {
+
+}
+
+enum ___VARIABLE_feature___Action {
+    
+}
+
+struct ___VARIABLE_feature___Environment {
+
+}
+
+let ___VARIABLE_feature:identifier___Reducer = Reducer<___VARIABLE_feature___State,___VARIABLE_feature___Action,___VARIABLE_feature___Environment> { state, action, environment in 
+
+	return .none
+
+}
